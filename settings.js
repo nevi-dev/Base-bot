6d838421a84536e90da9b156a4edfac395a5140e
Nevi <208419680+nevi-dev@users.noreply.github.com>
2025-12-05 00:53:19 -0400

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+// settings.js
+module.exports = {
+  NOMBRE_BOT: "BASE",
+  VERSION: "1.0.0",
+  PREFIJOS: ['.', '#', '!', '/'],
+  OWNER_NUMERO: '18096758983', // Tu número de desarrollador sin +
+  // Tiempo de limpieza de tmp en milisegundos (1 hora)
+  LIMPIEZA_TMP_MS: 3600000 
+};
