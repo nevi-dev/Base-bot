8dd992c092fc613cb6fc4e80beac5231148988c1
Nevi <208419680+nevi-dev@users.noreply.github.com>
2025-12-05 01:17:57 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,17 @@
 // settings.js
-module.exports = {
+
+const settings = {
+  // 🦈 Información General del Bot
   NOMBRE_BOT: "BASE",
   VERSION: "1.0.0",
+  
+  // 🛡️ Configuración de Comandos y Dueño
   PREFIJOS: ['.', '#', '!', '/'],
   OWNER_NUMERO: '18096758983', // Tu número de desarrollador sin +
-  // Tiempo de limpieza de tmp en milisegundos (1 hora)
+  
+  // ⏳ Gestión de Archivos
+  // Tiempo de limpieza de tmp en milisegundos (1 hora = 3,600,000 ms)
   LIMPIEZA_TMP_MS: 3600000 
 };
+
+export default settings;
